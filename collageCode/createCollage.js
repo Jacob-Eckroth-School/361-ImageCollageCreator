5a85163c12ba40c0d16f07ffaa77f075d2f67b67
JacobEckroth <jacobeckroth@gmail.com>
2021-05-03 20:49:47 -0700

    got collage creation workinggit add .git add .

--- collageCode/createCollage.js
+++ collageCode/createCollage.js
@@ -1,14 +1,15 @@
 
 const fs = require('fs')
 const { createCanvas,loadImage } = require('canvas')
+const { randomBytes, randomInt } = require('crypto')
 
 const width = 1200
-const height = 600
+const height = 1200
 
 const canvas = createCanvas(width, height)
 const context = canvas.getContext('2d')
 
-context.fillStyle = '#ff0000'
+context.fillStyle = '#ffffff'
 context.fillRect(0, 0, width, height)
 
 
@@ -16,27 +17,96 @@ var images = []
 
 async function loadImages(imagePathArray){
     for(let i = 0; i < imagePathArray.length; i++){
-        loadImage(imagePathArray[i]).then(image => {
+            await loadImage(imagePathArray[i]).then(image => {
             images.push(image);
-            console.log(images);
-            placeImageOnCanvas(images[i]);
+   
+          
         })
     }
+   
 
 }
 
-function placeImageOnCanvas(image){
-    console.log(image.width);
-    console.log(image.height);
-    console.log("Width - height ratio: " ,image.width/image.height);
+let imagePathArray = ["assets/1.jpeg","assets/2.jpeg","assets/3.jpeg","assets/4.jpeg","assets/5.jpeg","assets/6.jpeg","assets/1.jpeg","assets/2.jpeg","assets/3.jpeg","assets/4.jpeg","assets/5.jpeg","assets/6.jpeg","assets/1.jpeg","assets/2.jpeg","assets/3.jpeg","assets/4.jpeg","assets/5.jpeg","assets/6.jpeg","assets/1.jpeg","assets/2.jpeg","assets/3.jpeg","assets/4.jpeg","assets/5.jpeg","assets/6.jpeg"]
+
+async function createCanvasAsync(){
+   await loadImages(imagePathArray);
+   placeImagesOnCanvas();
+}
+
+
+function placeImagesOnCanvas(){
+    console.log(images);
+    let horizontalSections,verticalSections;
+    if(images.length < 3){
+        horizontalSections = images.length;
+    }else if(images.length < 8){
+        horizontalSections = 3.
+    }else{
+        horizontalSections = 4.
+    }
+    if(images.length < 6){
+        verticalSections = 1.;
+    }else if(images.length < 9){
+        verticalSections = 2.;
+    }else{
+        verticalSections = 3.;
+    }
+    let horizontalAdvance  = width / horizontalSections;
+    console.log("Horizontal advance: ",horizontalAdvance);
+    let verticalAdvance = height / verticalSections;
+    
+    var currentImageIndex = 0;
+    while(true){
+        for(var x = 0; x < horizontalSections; x++){
+            for(var y = 0; y < verticalSections; y++){
+                console.log("x:",x,"Y:",y);
+                var placeX = x * horizontalAdvance + randomInt(0,horizontalAdvance);
+                var placeY = y * verticalAdvance + randomInt(0,verticalAdvance);
+                var currentImage = images[currentImageIndex];
+           
+                var wToH = currentImage.width / currentImage.height;
+          
+                let newHeight, newWidth;
+                if(images.width > images.height){
+                    console.log("width > height");
+                    newWidth = horizontalAdvance *  (randomInt(7,10) * .1);
+                    
+                    newHeight = newWidth / wToH;
+                }else{
+                    console.log("height < width");
+                    newHeight = verticalAdvance  *  (randomInt(7,10) * .1); ;
+                    newWidth = newHeight * wToH;
+                }
+                if(placeX + newWidth > width){
+                    placeX = width - newWidth;
+                }
+                if(placeY + newHeight > height){
+                    placeY = height - newHeight;
+                }
+                console.log("Drawing with x: ",placeX," Y: ",placeY, "New Width: ",newWidth," and height:",newHeight);
+                context.drawImage(currentImage,placeX,placeY,newWidth,newHeight);
+                
+
+                currentImageIndex++;
+                if(currentImageIndex >= images.length){
+                    const buffer = canvas.toBuffer('image/png')
+                    fs.writeFileSync('./test2.png', buffer)
+                    return;
+                }
+            }
+            
+        }
+       
+    }
+ 
+
 
 }
 
 
-let imagePathArray = ["assets/1.jpeg","assets/2.jpeg","assets/3.jpeg"]
-
-loadImages(imagePathArray);
 
 
 
 
+createCanvasAsync();
