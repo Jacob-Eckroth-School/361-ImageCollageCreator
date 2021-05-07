53a969da0b9469426bc1aa721c1b5cb5d5061a37
JacobEckroth <jacobeckroth@gmail.com>
2021-05-06 20:38:36 -0700

    added uploading multiple images as well as the functionality to delete an image. Now ened to do cropping

--- public/imageDisplayTemplate.js
+++ public/imageDisplayTemplate.js
@@ -1,15 +1,19 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['imageDisplay'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
         }
         return undefined
     };
 
-  return "\r\n<div class = \"imageHolder\">  \r\n    <img class = \"animalPicture\" src=\""
-    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"animalImage") || (depth0 != null ? lookupProperty(depth0,"animalImage") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"animalImage","hash":{},"data":data,"loc":{"start":{"line":3,"column":38},"end":{"line":3,"column":53}}}) : helper)))
-    + "\" />\r\n</div>\r\n";
+  return "\r\n<div class = \"imageHolder\" id=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"imageID") || (depth0 != null ? lookupProperty(depth0,"imageID") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"imageID","hash":{},"data":data,"loc":{"start":{"line":2,"column":31},"end":{"line":2,"column":42}}}) : helper)))
+    + "\">  \r\n    <img class = \"animalPicture\" src=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"animalImage") || (depth0 != null ? lookupProperty(depth0,"animalImage") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"animalImage","hash":{},"data":data,"loc":{"start":{"line":3,"column":38},"end":{"line":3,"column":53}}}) : helper)))
+    + "\" alt=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"alt") || (depth0 != null ? lookupProperty(depth0,"alt") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"alt","hash":{},"data":data,"loc":{"start":{"line":3,"column":60},"end":{"line":3,"column":67}}}) : helper)))
+    + "\" />\r\n    <div class=\"buttonsHolder\">\r\n        <button class=\"crop alterButton\" >Crop</button>\r\n        <button class=\"delete alterButton\">Delete</button>\r\n    </div>\r\n</div>\r\n";
 },"useData":true});
 })();
--- public/imageUploadingScript.js
+++ public/imageUploadingScript.js
@@ -1,15 +1,71 @@
 // import 'cropperjs/dist/cropper.css';
 //import Cropper from '/cropperjs/cropperjs';////
 
-const image = document.getElementById('image');
-const cropper = new Cropper(image, {
-  viewMode:1,
-  movable:false,
-  zoomable:false,
-  minCropBoxWidth:100,
-  minCropBoxHeight:100
+var images = {}
+
+var imageCount = 0;
+
+var uploadButton = document.getElementById("uploadButton")
+
+
+uploadButton.addEventListener("click",uploadImage);
+var imgUpload = document.getElementById("imgupload");
+imgUpload.addEventListener('change',function(){
+    if(this.files && this.files[0]){
+        if(!(this.files[0].type.match('image.*'))){
+            alert("Please only upload image files") //make this look nice later
+            return;
+        }
+        src = URL.createObjectURL(this.files[0]); // set src to blob url
+        var newID = this.files[0].name + imageCount + Math.floor(Math.random() * 10000)
+        images[newID] = src;
+    
+        
+
+        var context ={
+            alt:"A Picture",
+            animalImage:images[newID],
+            imageID:newID
+        }
+        imageCount++;
+        var photoHTML = Handlebars.templates.imageDisplay(context);
+        
+        document.getElementById("photoHolder").insertAdjacentHTML('beforeend',photoHTML);
+        
+        var insertedImage = document.getElementById("photoHolder").children[document.getElementById("photoHolder").children.length -1];
+        var buttonsHolder = insertedImage.children[insertedImage.children.length-1];
+        var cropButton = buttonsHolder.children[0];
+        var deleteButton=buttonsHolder.children[1];
+        deleteButton.addEventListener("click",function(){
+            attemptDelete(newID)
+        });
+        cropButton.addEventListener("click",function(){
+            attemptCrop(newID);
+        });
+    
+
+        
+        imgUpload.value='';
+    }
 });
 
+function attemptDelete(imageID){
 
+   
+   
+    var removingElement = document.getElementById(imageID);
 
+    document.getElementById("photoHolder").removeChild(removingElement);
+    imageCount--;
+    images[imageID] = null;
+}
+function attemptCrop(imageID){
+    console.log("Crop ",document.getElementById(imageID));
+}
+
+function uploadImage(){
+    
+    imgUpload.click();
+
+}
 //https://stackabuse.com/handling-file-uploads-in-node-js-with-expres-and-multer/
