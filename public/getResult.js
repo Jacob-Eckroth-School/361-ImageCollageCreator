00d843043197aa40bab2d010f3a65c0c709a4fc7
JacobEckroth <jacobeckroth@gmail.com>
2021-05-10 18:03:54 -0700

    made go back button work

--- public/getResult.js
+++ public/getResult.js
@@ -10,3 +10,8 @@ function loadResult(title){
     var resultHTML = Handlebars.templates.resultDisplay(context);
     document.getElementById("goAgainHolder").insertAdjacentHTML('beforebegin',resultHTML);
 }
+
+
+document.getElementById("goAgainButton").addEventListener("click",function(){
+    window.location.href="/"
+})
